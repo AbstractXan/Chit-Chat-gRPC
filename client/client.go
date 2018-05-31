054aa76d9042bf4ed55688096baf9e31c5318f22
Priyansh Sangule <priyansh0506@gmail.com>
2018-05-31 11:19:23 +0530

    Add files via upload

--- client/client.go
+++ client/client.go
@@ -1,39 +1,89 @@
 package main
 
 import (
-	pb "chat/chat"
+	"bufio"
+	"fmt"
 	"log"
-	"net"
-	"time"
+	"os"
+
+	"google.golang.org/grpc"
 
 	"golang.org/x/net/context"
-	"google.golang.org/grpc"
+
+	pb "grpcgittest/proto"
 )
 
-func connect(client pb.ChatClient, request *pb.Request) bool {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	resp, err := client.ConnRequest(ctx, request)
-	if err != nil {
-
-		log.Fatalf("%v.connect(_) = _, %v", client, err)
-		return false
+func listenToClient(sendQ chan pb.Message, reader *bufio.Reader, name string) {
+	for {
+		msg, _ := reader.ReadString('\n')
+		sendQ <- pb.Message{Sender: name, Text: msg}
 	}
-	log.Println(resp)
-	return true
 }
-func main() {
-	serverAddr := net.JoinHostPort("localhost", "8080")
 
-	//setup insecure connection
-	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
-	if err != nil {
-		log.Fatal(err)
+func receiveMessages(stream pb.Chat_TransferMessageClient, mailbox chan pb.Message) {
+	for {
+		msg, _ := stream.Recv()
+		mailbox <- *msg
 	}
-	client := pb.NewChatClient(conn)
-	ok := connect(client, &pb.Request{Username: "Xan", Password: "123"})
-	if ok == false {
-		return
-	} //Exits if cannot connect
+}
 
+// Connect : Connects to server
+func Connect(address string) error {
+
+	//Dial to server
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	//Setup new client
+	client := pb.NewChatClient(conn)
+
+	//Recieve stream
+	stream, err := client.TransferMessage(context.Background())
+	if err != nil {
+		return err
+	}
+
+	//Input name to construct Message
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your name: ")
+	clientName, err := reader.ReadString('\n')
+
+	if err != nil {
+		return err
+	}
+
+	//Send initial message with Sender and Register=TRUE
+	stream.Send(&pb.Message{Sender: clientName, Register: true})
+
+	//Make buffered mailbox recieve message from server
+	mailBox := make(chan pb.Message, 100)
+	go receiveMessages(stream, mailBox)
+
+	//Make send queue buffered message
+	sendQ := make(chan pb.Message, 100)
+	go listenToClient(sendQ, reader, clientName)
+
+	//Forever
+	for {
+		select {
+
+		//If send channel is active, send to server
+		case toSend := <-sendQ:
+			stream.Send(&toSend)
+
+		//If mailbox has something, print.
+		case received := <-mailBox:
+			fmt.Printf("%s> %s", received.Sender, received.Text)
+		}
+	}
+}
+
+func main() {
+	err := Connect("127.0.0.1:10000")
+	if err != nil {
+		log.Println(err)
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,48 +1,130 @@
 package main
 
 import (
-	"log"
-	"golang.org/x/net/context"
-	"google.golang.org/grpc"
-	//"google.golang.org/grpc/codes"
-	//"google.golang.org/grpc/status"
+	"fmt"
+	"io"
 	"net"
+	"sync"
 
-	pb "chat/chat"
+	"google.golang.org/grpc"
+
+	pb "grpcgittest/proto"
 )
 
-type server struct{
-	UserDB map[string]string //Username and their pasword
-	//UserChan map[string]chan string
-	GroupDB []group
+var usersLock = &sync.Mutex{}
+
+var usersMap = make(map[string]chan pb.Message, 100)
+
+type chatServer struct {
 }
 
-type group struct{
-	Name string
-	//Channels slice with text ('string') type
-	// !!!Channels should be bufferd or else they'd deadlock !!!
-	Channels []chan string 
+func newChatServer() *chatServer {
+	return &chatServer{}
 }
 
-var port = ":8080"
-
-func (s *server) ConnRequest(ctx context.Context, in *pb.Request) (*pb.Confirm, error) {
-	n, d := in.Username, in.Password
-	log.Printf("%s : %s connected!",n,d)
-	//Temporary
-	return &pb.Confirm{Ok: true}, nil
+func addListener(name string, msgQ chan pb.Message) {
+	usersLock.Lock()
+	defer usersLock.Unlock()
+	usersMap[name] = msgQ
 }
 
-//Broadcast function
-func (s *server) Broadcast(stream pb.Chat_BroadcastServer) error{
+func removeListener(name string) {
+	usersLock.Lock()
+	defer usersLock.Unlock()
+	delete(usersMap, name)
+}
+
+func hasListener(name string) bool {
+	usersLock.Lock()
+	defer usersLock.Unlock()
+	_, exists := usersMap[name]
+	return exists
+}
+
+func broadcast(sender string, msg pb.Message) {
+	usersLock.Lock()
+	defer usersLock.Unlock()
+	for user, q := range usersMap {
+		if user != sender {
+			q <- msg
+		}
+	}
+}
+
+//When messages come from Client stream
+func listenToClient(stream pb.Chat_TransferMessageServer, messages chan<- pb.Message) {
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			// ?
+		}
+		if err != nil {
+			// ??
+		}
+		messages <- *msg
+	}
+}
+
+//Transfer message over stream address
+func (s *chatServer) TransferMessage(stream pb.Chat_TransferMessageServer) error {
+
+	//Recieve from stream
+	InMessage, err := stream.Recv()
+
+	//Variable for client name
+	var clientName string
+
+	//Make Client Buffer Mailbox
+	clientMailbox := make(chan pb.Message, 100)
+
+	if err != nil {
+		return err
+	}
+
+	//If Register == TRUE
+	if InMessage.Register {
+
+		//Set Client name as in MESAAGE
+		clientName = InMessage.Sender
+
+		//Check if HasListener, i.e. has a channel
+		if hasListener(clientName) {
+			return fmt.Errorf("name already exists")
+		}
+
+		//Else add listener, i.e. new channel
+		addListener(clientName, clientMailbox)
+	} else {
+		return fmt.Errorf("need to register first")
+	}
+
+	//Create a listener to client
+	clientMessages := make(chan pb.Message, 100)
+	go listenToClient(stream, clientMessages)
+
+	for {
+		select {
+		case messageFromClient := <-clientMessages:
+			broadcast(clientName, messageFromClient)
+		case messageFromOthers := <-clientMailbox:
+			stream.Send(&messageFromOthers)
+		}
+	}
+}
+
+//Serve : Serves at specific address
+func Serve() error {
+
+	lis, err := net.Listen("tcp", ":10000")
+	if err != nil {
+		return err
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterChatServer(grpcServer, newChatServer())
+	grpcServer.Serve(lis)
 	return nil
 }
+
 func main() {
-	for{
-	lis, _ := net.Listen("tcp", port)
-	//MIGHT NOT NEED GO ROUTINE
-		s := grpc.NewServer()
-		pb.RegisterChatServer(s, &server{UserDB: make(map[string]string)})
-		s.Serve(lis)
-	}
+	Serve()
 }
